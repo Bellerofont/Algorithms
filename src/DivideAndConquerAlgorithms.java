4f52cc6d501c2e109fe10ed71bad88ff8a62b775
Bellerofont <lord-pavlo@yandex.ua>
2018-10-20 00:44:00 +0300

    add majorityElement method

--- src/DivideAndConquerAlgorithms.java
+++ src/DivideAndConquerAlgorithms.java
@@ -1,5 +1,7 @@
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class DivideAndConquerAlgorithms {
@@ -17,14 +19,56 @@ public class DivideAndConquerAlgorithms {
         for (int i = 0; i < k; i++) {
             keys[i] = scanner.nextInt();
         }
+        long start = System.nanoTime();
         System.out.println(binarySearch(n, sequence, k, keys));
+        long end = System.nanoTime();
+        System.out.println("run time: " + (end-start)/1000000 + "ms");
+
+        File input2 = new File("4_2_majority_element.in");
+        scanner = new Scanner(input2);
+        n = scanner.nextInt();
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+        list.add(scanner.nextInt());
+        }
+        start = System.nanoTime();
+        System.out.println(majorityElement( n, list));
+        end = System.nanoTime();
+        System.out.println("run time: " + (end-start)/1000000 + "ms");
+    }
+
+
+    private static int majorityElement(int n, List<Integer> list) {
+        if(list.size()==0){
+            return -1;
+        }
+        if(list.size()==1){
+            return list.get(0);
+        }
+        List<Integer> processedList = new ArrayList<>();
+        if(n%2!=0){
+            if(!list.get(n-1).equals(list.get(n-2))&&!list.get(n-1).equals(list.get(n-3))){
+                n--;
+                list.remove(n);
+            }
+            else{
+                list.add(list.get(n-1));
+                n++;
+            }
+        }
+        for (int i = 0; i < n; i+=2) {
+            if(list.get(i).equals(list.get(i+1))){
+                processedList.add(list.get(i));
+            }
+        }
+        return majorityElement(processedList.size(),processedList);
     }
 
     private static int binarySearch(int n, int[] sequence, int k, int[] keys) {
         int hits = 0;
         int [] indexes = new int[k];
         for (int i = 0; i < k; i++) {
-            indexes[i] = binSearch(0, n, sequence, keys[i]);
+            indexes[i] = binarySearch(0, n, sequence, keys[i]);
             if(indexes[i]!=-1){
                 hits++;
             }
@@ -32,7 +76,7 @@ public class DivideAndConquerAlgorithms {
         return hits;
     }
 
-    private static int binSearch(int i, int n, int[] sequence, int key) {
+    private static int binarySearch(int i, int n, int[] sequence, int key) {
         if (i>n){
             return -1;
         }
@@ -41,10 +85,10 @@ public class DivideAndConquerAlgorithms {
             return mid;
         }
         else if (sequence[mid]>key){
-            return binSearch(i, mid-1,sequence, key);
+            return binarySearch(i, mid-1,sequence, key);
         }
         else {
-            return binSearch(mid+1,n,sequence,key);
+            return binarySearch(mid+1,n,sequence,key);
         }
     }
 }
