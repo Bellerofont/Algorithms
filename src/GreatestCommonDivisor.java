50f8038bcf606d8692accd7de396d861ee1122b4
Pavlo Bagriy <bagriy.pavlo@gmail.com>
2020-08-11 10:47:40 +0300

    Create GreatestCommonDivisor.java
    Copied from old repository

---
+++ src/GreatestCommonDivisor.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class GreatestCommonDivisor {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        long a = scanner.nextInt();
+        long b = scanner.nextInt();
+        System.out.println(findGCD(a,b));
+        //System.out.println(findLCM(a,b));
+    }
+
+    private static long findGCD(long a, long b) {
+        long max = Math.max(a,b);
+        long min = Math.min(a,b);
+        if(max%min==0) return min;
+        return findGCD(max%min,min);
+    }
+
+    private static long findLCM(long a,long b){
+        return a*b/findGCD(a,b);
+    }
+}
